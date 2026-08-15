822b6f230636d32f863bb117bb2e0b8c8558ca7b
Mackey <noctissaragih@gmail.com>
2026-08-15 21:05:34 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const targetId = link.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+            });
+        }
+    });
+});
